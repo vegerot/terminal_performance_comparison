19e3c2a159147a95c3b7c82fae9bd13d6036b9a8
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2023-08-28 20:19:23 -0700

    comment out leaking file handles case

--- main.go
+++ main.go
@@ -50,36 +50,33 @@ func main() {
 }
 
 func benchmark(N int) {
-	{
-		now := time.Now()
-		for i := 0; i < N; i++ {
-			OpenAllFilesNoDefer()
-		}
-		durationPerRun := time.Since(now) / time.Duration(N)
-		fmt.Printf("OpenAllFilesNoDefer: %s\n", durationPerRun)
+	now := time.Now()
+	for i := 0; i < N; i++ {
+		OpenAllFilesNoDefer()
 	}
+	durationPerRun := time.Since(now) / time.Duration(N)
+	fmt.Printf("OpenAllFilesNoDefer: %s\n", durationPerRun)
 
-	{
-		now := time.Now()
-		for i := 0; i < N; i++ {
-			OpenAllFilesDefer()
-		}
-		durationPerRun := time.Since(now) / time.Duration(N)
-		fmt.Printf("OpenAllFilesDefer: %s\n", durationPerRun)
+	now = time.Now()
+	for i := 0; i < N; i++ {
+		OpenAllFilesDefer()
 	}
+	durationPerRun = time.Since(now) / time.Duration(N)
+	fmt.Printf("OpenAllFilesDefer: %s\n", durationPerRun)
 
 	// NOTE: OpenAllFilesDefer and OpenAllFilesLeakFileHandles have the same
-	// speed, but it depends on which order you run them in.  Rearrange these
-	// two blocks and you'll see their results swap
+	// speed, but it depends on which order you run them in.  Uncomment just
+	// one of these blocks and you'll see the same speed
 
-	{
+	/*{
 		now := time.Now()
 		for i := 0; i < N; i++ {
 			OpenAllFilesLeakFileHandles()
 		}
 		durationPerRun := time.Since(now) / time.Duration(N)
 		fmt.Printf("OpenAllFilesLeakFileHandles: %s\n", durationPerRun)
-	}
+	}*/
+
 }
 
 func createFiles(n int) {
